ccdf597437943582a402b6f4ae42187e1d58d061
Christian Koernig <christian.koernig@esa.int>
2018-11-30 14:43:44 +0100

    Testing Invisible Mask shader

---
+++ unity-arkit-plugin/Assets/UnityARKitPlugin/Examples/ARKit2.0/UnityARWorldMap/Scripts/Obscurable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obscurable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Renderer[] renders = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rendr in renders)
+        {
+            rendr.material.renderQueue = 2002; // set their renderQueue
+        }
+    }
+
+}
